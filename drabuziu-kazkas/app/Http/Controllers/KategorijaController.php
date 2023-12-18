64d68d0cba2653f1928f77b731e13957f75c4b5e
ArijusVilkelis <arivil1@ktu.lt>
2023-12-18 18:24:20 +0200

    pavyko nx

--- drabuziu-kazkas/app/Http/Controllers/KategorijaController.php
+++ drabuziu-kazkas/app/Http/Controllers/KategorijaController.php
@@ -4,49 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use App\Models\Kategorija;
 use Illuminate\View\View;
+use App\Models\Kategorija;
 
 class KategorijaController extends Controller
 {
-
-    private $conn;
-    public function __construct()
-    {
-        // Database connection parameters
-        $servername = "localhost";
-        $username = "root";
-        $password = ""; // Replace with your actual database password
-        $dbname = "parde"; // Replace with your actual database name
-
-        // Create connection
-        $this->conn = new \mysqli($servername, $username, $password, $dbname);
-
-        // Check connection
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-    }
-
     public function index(): View
     {
         $kategorijos = Kategorija::all();
-        return view ('kategorijos.index')->with('kategorijos', $kategorijos);
+        return view('kategorijos.index')->with('kategorijos', $kategorijos);
     }
 
- 
     public function create(): View
     {
         return view('kategorijos.create');
     }
 
-  
     public function store(Request $request): RedirectResponse
     {
         $input = $request->all();
         Kategorija::create($input);
-        return redirect('kategorija')->with('flash_message', 'Student Addedd!');
+        return redirect('kategorija')->with('flash_message', 'Student Added!');
     }
 
     public function show(string $id): View
@@ -66,18 +44,12 @@ class KategorijaController extends Controller
         $kategorija = Kategorija::find($id);
         $input = $request->all();
         $kategorija->update($input);
-        return redirect('kategorija')->with('flash_message', 'student Updated!');  
+        return redirect('kategorija')->with('flash_message', 'student Updated!');
     }
 
-    
     public function destroy(string $id): RedirectResponse
     {
         Kategorija::destroy($id);
-        return redirect('kategorija')->with('flash_message', 'Student deleted!'); 
-    }
-    public function __destruct()
-    {
-        // Close the database connection when the controller is destroyed
-        $this->conn->close();
+        return redirect('kategorija')->with('flash_message', 'Student deleted!');
     }
 }
--- drabuziu-kazkas/app/Models/Kategorija.php
+++ drabuziu-kazkas/app/Models/Kategorija.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kategorija extends Model
 {
-    protected $connection = 'parde'; // specify the database connection
+    protected $connection = 'mysql'; // specify the database connection
     protected $table = 'kategorijos'; // specify the table name if it's different from the model name
     protected $primaryKey = 'id_Kategorija'; // specify the primary key if it's different
     protected $fillable = ['pavadinimas', 'aprasymas','fk_Kategorijaid_Kategorija'];
