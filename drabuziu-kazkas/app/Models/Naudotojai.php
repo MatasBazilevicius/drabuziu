8f3fd26b523e3a77d4e3ad88d00cc84e491cc6cb
MatasBazilevicius <matas.bazilevicius@ktu.edu>
2023-12-18 03:10:24 +0200

    naudotojai su useriais

--- drabuziu-kazkas/app/Models/Naudotojai.php
+++ drabuziu-kazkas/app/Models/Naudotojai.php
@@ -10,47 +10,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Naudotojai extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    protected $table = 'naudotojai';
+    protected $fillable = ['id_Naudotojas', 'Vardas', 'El_pastas'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */ 
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public $timestamps = false;
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
-
-    /**
-     * Send the password reset notification.
-     *
-     * @param  string  $token
-     * @return void
-     */
-    public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new CustomResetPasswordNotification($token));
-    }
 }
