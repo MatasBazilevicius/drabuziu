bd30434b5caa27875134adfb659921d4a46bd615
MatasBazilevicius <matas.bazilevicius@ktu.edu>
2024-01-02 19:12:44 +0200

    patvarkytas uzsakymai table

--- drabuziu-kazkas/app/Models/Uzsakymai.php
+++ drabuziu-kazkas/app/Models/Uzsakymai.php
@@ -10,9 +10,9 @@ class Uzsakymai extends Model
     public $timestamps = false;
     protected $connection = 'mysql'; // specify the database connection
     protected $table = 'uzsakymai'; // specify the table name if it's different from the model name
-    protected $primaryKey = 'Uzsakymo_num'; // specify the primary key if it's different
+    protected $primaryKey = 'id_Uzsakymas'; // specify the primary key if it's different
     protected $fillable = [
-        'Uzsakymo_num', 'suma', 'Vardas', 'Pavarde', 'Gatves_adresas',
+        'suma', 'Vardas', 'Pavarde', 'Gatves_adresas',
         'Miestas', 'Pasto_kodas', 'Pristatymo_salis', 'pristatymo_budas',
         'busena', 'id_Uzsakymas', 'fk_Krepselisid_Krepselis',
         'fk_Nuolaidų_Kodaiid_Nuolaidų_Kodai', 'fk_Apmokejimasid_Apmokejimas'
